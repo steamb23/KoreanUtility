28f303156760ad5d1206b54ee097a7c4232b8163
steamb23 <steamb23@gmail.com>
2015-01-18 14:49:38 +0900

    자소 분리 기능 추

---
+++ KoreanUtility/Hangul/Phoneme.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamB23.KoreanUtility.Hangul
+{
+    public struct Phoneme
+    {
+        internal Phoneme(char source, char initialConsonant, char vowel, char finalConsonant, byte initialConsonantNumber, byte vowelNumber, byte finalConsonantNumber)
+        {
+            this.source = source;
+            this.initialConsonant = initialConsonant;
+            this.vowel = vowel;
+            this.finalConsonant = finalConsonant;
+            this.initialConsonantNumber = initialConsonantNumber;
+            this.vowelNumber = vowelNumber;
+            this.finalConsonantNumber = finalConsonantNumber;
+        }
+        public char source;
+        public char initialConsonant;
+        public char vowel;
+        public char finalConsonant;
+        public byte initialConsonantNumber;
+        public byte vowelNumber;
+        public byte finalConsonantNumber;
+    }
+}
---
+++ KoreanUtility/Hangul/PhonemeSeparation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamB23.KoreanUtility.Hangul
+{
+    /// <summary>
+    /// 자소를 분리하는 클래스입니다.
+    /// </summary>
+    public static class PhonemeSeparation
+    {
+        public static Phoneme GetPhoneme(char character)
+        {
+            int tempCharacter = character - 0xac00;
+            int tempInitialConsonant = tempCharacter / (21 * 28);
+            int tempVowel = (tempCharacter % (21 * 28)) / 28;
+            int tempFinalConsonant = (tempCharacter % (21 * 28)) % 28;
+
+            char initialConsonant = (char)(tempInitialConsonant + 0x1100);
+            char vowel = (char)(tempVowel + 0x1161);
+            char finalConsonant = (char)(tempFinalConsonant + 0x11a7);
+
+            return new Phoneme(character, initialConsonant, vowel, finalConsonant, (byte)tempInitialConsonant, (byte)tempFinalConsonant, (byte)tempFinalConsonant);
+        }
+        public static Phoneme[] GetPhonemes(char[] characters)
+        {
+            Phoneme[] phonemes = new Phoneme[characters.Length];
+            int fcount=0;
+            foreach (var ftemp in characters)
+            {
+                phonemes[fcount] = GetPhoneme(ftemp);
+                fcount++;
+            }
+            return phonemes;
+        }
+        public static Phoneme[] GetPhonemes(string text)
+        {
+            return GetPhonemes(text.ToCharArray());
+        }
+    }
+}
