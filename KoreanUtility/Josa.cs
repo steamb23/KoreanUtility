94e1135a0453fbc6ab47175d060b5f426a5e5eb9
steamb23 <steamb23@gmail.com>
2015-01-18 13:54:01 +0900

    한글 텍스트 환경에서의 빠른 사용을 위해서 공개된 요소를 한국어로 바꿈

--- KoreanUtility/Josa.cs
+++ KoreanUtility/Josa.cs
@@ -16,22 +16,22 @@ namespace SteamB23.KoreanUtility
         /// <param name="text">대상 문자열입니다.</param>
         /// <param name="type">추가될 조사의 타입입니다.</param>
         /// <returns>조사가 추가된 문자열입니다.</returns>
-        public static string AppendJosa(this string text, JosaType type)
+        public static string 조사추가(this string text, 조사 type)
         {
             char lastChar = char.Parse(text.Substring(text.Length - 1, 1));
-            return text + DecideJosa(lastChar, type);
+            return text + 조사결정(lastChar, type);
         }
         /// <summary>
         /// 한국어에서의 받침 유무에 따라 바뀌는 조사를 자동으로 추가합니다.
         /// </summary>
         /// <param name="textBuilder">대상 <c>StringBuilder</c>입니다.</param>
         /// <param name="type">추가될 조사의 타입입니다.</param>
-        public static void AppendJosa(this StringBuilder textBuilder, JosaType type)
+        public static void 조사추가(this StringBuilder textBuilder, 조사 type)
         {
             string text = textBuilder.ToString();
 
             char lastChar = char.Parse(text.Substring(text.Length - 1, 1));
-            textBuilder.Append(DecideJosa(lastChar, type));
+            textBuilder.Append(조사결정(lastChar, type));
         }
         /// <summary>
         /// 마지막 글자의 받침 유무에 따라 바뀌는 조사를 결정합니다. 
@@ -39,7 +39,7 @@ namespace SteamB23.KoreanUtility
         /// <param name="lastChar">확인할 글자입니다.</param>
         /// <param name="type">결정될 조사의 타입입니다.</param>
         /// <returns>결정된 조사입니다.</returns>
-        public static string DecideJosa(char lastChar, JosaType type)
+        public static string 조사결정(char lastChar, 조사 type)
         {
             if ((lastChar <= '가' && lastChar >= '힣'))
             {
@@ -47,19 +47,19 @@ namespace SteamB23.KoreanUtility
             }
             switch (type)
             {
-                case JosaType.은_는:
+                case 조사.은_는:
                     return GenericJosaRule(lastChar, "은", "는");
-                case JosaType.이_가:
+                case 조사.이_가:
                     return GenericJosaRule(lastChar, "이", "가");
-                case JosaType.을_를:
+                case 조사.을_를:
                     return GenericJosaRule(lastChar, "을", "를");
-                case JosaType.과_와:
+                case 조사.과_와:
                     return GenericJosaRule(lastChar, "과", "와");
-                case JosaType.아_야:
+                case 조사.아_야:
                     return GenericJosaRule(lastChar, "아", "야");
-                case JosaType.이다_다:
+                case 조사.이다_다:
                     return GenericJosaRule(lastChar, "이", "");
-                case JosaType.으로_로:
+                case 조사.으로_로:
                     if (((lastChar - 0xAC00) % (21 * 28)) % 28 == 0 || ((lastChar - 0xAC00) % (21 * 28)) % 28 == 5)
                         return "로";
                     else
@@ -86,9 +86,9 @@ namespace SteamB23.KoreanUtility
     /// <summary>
     /// 조사와 관련된 메서드에 대한 조사의 종류를 정의합니다.
     /// </summary>
-    public enum JosaType
+    public enum 조사
     {
-        // 빠른 사용을 위한 값
+        // 빠른 자동 완성 사용을 위한 값
         /// <summary>
         /// 보조사 : 은/는
         /// </summary>
@@ -119,6 +119,38 @@ namespace SteamB23.KoreanUtility
         /// (경고 : ㄹ받침은 받침이 없는 경우와 같습니다.)
         /// </summary>
         _으로 = 6,
+
+        // 빠른 작성을 위한 값
+        /// <summary>
+        /// 보조사 : 은/는
+        /// </summary>
+        은는 = 0,
+        /// <summary>
+        /// 주격조사 : 이/가
+        /// </summary>
+        이가 = 1,
+        /// <summary>
+        /// 목적격 조사 : 을/를
+        /// </summary>
+        을를 = 2,
+        /// <summary>
+        /// 접속조사 : 과/와
+        /// </summary>
+        과와 = 3,
+        /// <summary>
+        /// 호격 조사 : 아/야
+        /// </summary>
+        아야 = 4,
+        /// <summary>
+        /// 서술격 조사 : 이다/다
+        /// (경고 : 이다/다, 이고/고, 이니/고, 이면/면, 이지/지, 이여/여 등으로 변형 사례가 많기 때문에 '이'와 ''를 리턴합니다.)
+        /// </summary>
+        이 = 5,
+        /// <summary>
+        /// 부사격 조사 : 으로/로,
+        /// (경고 : ㄹ받침은 받침이 없는 경우와 같습니다.)
+        /// </summary>
+        으로 = 6,
 
         // 가독성을 위한 값
         /// <summary>
--- KoreanUtilityTest/JosaClass.cs
+++ KoreanUtilityTest/JosaClass.cs
@@ -8,36 +8,36 @@ namespace KoreanUtilityTest
     public class JosaClass
     {
         [TestMethod]
-        public void AppendJosa()
+        public void 조사추가()
         {
             // 은/는
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.EN) + " 맛있다.", "사과는 맛있다.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.EN) + " 맛있다.", "단감은 맛있다.");
+            Assert.AreEqual<string>("사과".조사추가(조사.은는) + " 맛있다.", "사과는 맛있다.");
+            Assert.AreEqual<string>("단감".조사추가(조사.은는) + " 맛있다.", "단감은 맛있다.");
             // 이/가
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.IG) + " 좋다.", "사과가 좋다.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.IG) + " 좋다.", "단감이 좋다.");
+            Assert.AreEqual<string>("사과".조사추가(조사.이가) + " 좋다.", "사과가 좋다.");
+            Assert.AreEqual<string>("단감".조사추가(조사.이가) + " 좋다.", "단감이 좋다.");
             // 을/를
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.ER) + " 먹는다.", "사과를 먹는다.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.ER) + " 먹는다.", "단감을 먹는다.");
+            Assert.AreEqual<string>("사과".조사추가(조사.을를) + " 먹는다.", "사과를 먹는다.");
+            Assert.AreEqual<string>("단감".조사추가(조사.을를) + " 먹는다.", "단감을 먹는다.");
             // 와/과
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.GW) + " 귤.", "사과와 귤.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.GW) + " 귤.", "단감과 귤.");
+            Assert.AreEqual<string>("사과".조사추가(조사.과와) + " 귤.", "사과와 귤.");
+            Assert.AreEqual<string>("단감".조사추가(조사.과와) + " 귤.", "단감과 귤.");
             // 아/야
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.AY) + ", 넌 어찌 이리도 맛있니.", "사과야, 넌 어찌 이리도 맛있니.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.AY) + ", 넌 어찌 이리도 맛있니.", "단감아, 넌 어찌 이리도 맛있니.");
+            Assert.AreEqual<string>("사과".조사추가(조사.아야) + ", 넌 어찌 이리도 맛있니.", "사과야, 넌 어찌 이리도 맛있니.");
+            Assert.AreEqual<string>("단감".조사추가(조사.아야) + ", 넌 어찌 이리도 맛있니.", "단감아, 넌 어찌 이리도 맛있니.");
             // 이여/여
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.IDD) + "여, 그대는 먹히기 위한 존재.", "사과여, 그대는 먹히기 위한 존재.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.IDD) + "여, 그대는 먹히기 위한 존재.", "단감이여, 그대는 먹히기 위한 존재.");
+            Assert.AreEqual<string>("사과".조사추가(조사.이) + "여, 그대는 먹히기 위한 존재.", "사과여, 그대는 먹히기 위한 존재.");
+            Assert.AreEqual<string>("단감".조사추가(조사.이) + "여, 그대는 먹히기 위한 존재.", "단감이여, 그대는 먹히기 위한 존재.");
             // 이다/다
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.IDD) + "다.", "사과다.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.IDD) + "다.", "단감이다.");
+            Assert.AreEqual<string>("사과".조사추가(조사.이) + "다.", "사과다.");
+            Assert.AreEqual<string>("단감".조사추가(조사.이) + "다.", "단감이다.");
             // 으로/로
-            Assert.AreEqual<string>("사과".AppendJosa(JosaType.IDD) + " 장난치지마.", "사과로 장난치지마.");
-            Assert.AreEqual<string>("단감".AppendJosa(JosaType.IDD) + " 장난치지마.", "단감으로 장난치지마.");
+            Assert.AreEqual<string>("사과".조사추가(조사.으로) + " 장난치지마.", "사과로 장난치지마.");
+            Assert.AreEqual<string>("단감".조사추가(조사.으로) + " 장난치지마.", "단감으로 장난치지마.");
             // 한글외 언어 인식 테스트
-            Assert.AreEqual<string>("ABC".AppendJosa(JosaType.EN) + " 글자다.", "ABC은 글자다.");
-            // Josa클래스 직접 접근
-            Assert.AreEqual<string>(Josa.AppendJosa("나", JosaType.EN) + " 테스트 중이다.", "나는 테스트 중이다.");
+            Assert.AreEqual<string>("ABC".조사추가(조사.은는) + " 글자다.", "ABC은 글자다.");
+            // Josa클래스 직접 사용
+            Assert.AreEqual<string>(Josa.조사추가("나", 조사.은는) + " 테스트 중이다.", "나는 테스트 중이다.");
         }
     }
 }
