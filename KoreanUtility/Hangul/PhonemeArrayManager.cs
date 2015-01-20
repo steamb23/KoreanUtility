b23c64bcef98eb1f4198c494ecf60bda616a9983
steamb23 <steamb23@gmail.com>
2015-01-20 16:23:40 +0900

    PhonemeArrayManager클래스 추가 '#4'의 일부

---
+++ KoreanUtility/Hangul/PhonemeArrayManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamB23.KoreanUtility.Hangul
+{
+    public static class PhonemeArrayManager
+    {
+        /// <summary>
+        /// Phoneme배열에서 source 문자들을 조합한 문자열을 가져옵니다.
+        /// </summary>
+        /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
+        /// <returns>source 문자들을 조합한 문자열입니다.</returns>
+        public static string GetSourceString(this Phoneme[] phonemes)
+        {
+            StringBuilder tempStringBuilder = new StringBuilder();
+            foreach (var ftemp in phonemes)
+            {
+                tempStringBuilder.Append(ftemp.source);
+            }
+            return tempStringBuilder.ToString();
+        }
+        /// <summary>
+        /// Phoneme배열에서 초,중,종성 문자를 조합한 문자열을 가져옵니다.
+        /// </summary>
+        /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
+        /// <returns>source 문자들을 조합한 문자열입니다.</returns>
+        public static string GetPhonemeString(this Phoneme[] phonemes)
+        {
+            StringBuilder tempStringBuilder = new StringBuilder();
+            foreach (var ftemp in phonemes)
+            {
+                if (ftemp.initial == '\0' || ftemp.peak == '\0' || ftemp.final == '\0')
+                {
+                    tempStringBuilder.Append(ftemp.initial);
+                    tempStringBuilder.Append(ftemp.peak);
+                    tempStringBuilder.Append(ftemp.final);
+                }
+                else
+                    tempStringBuilder.Append(ftemp.source);
+            }
+            return tempStringBuilder.ToString();
+        }
+        /// <summary>
+        /// Phoneme배열에서 초,중,종성 문자를 조합한 문자열을 가져옵니다.
+        /// </summary>
+        /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
+        /// <returns>source 문자들을 조합한 문자열입니다.</returns>
+        public static string GetPhonemeString(this Phoneme[] phonemes)
+        {
+            StringBuilder tempStringBuilder = new StringBuilder();
+            foreach (var ftemp in phonemes)
+            {
+                if (ftemp.initial == '\0' || ftemp.peak == '\0' || ftemp.final == '\0')
+                {
+                    tempStringBuilder.Append(ftemp.initial);
+                    tempStringBuilder.Append(ftemp.peak);
+                    tempStringBuilder.Append(ftemp.final);
+                }
+                else
+                    tempStringBuilder.Append(ftemp.source);
+            }
+            return tempStringBuilder.ToString();
+        }
+    }
+}
