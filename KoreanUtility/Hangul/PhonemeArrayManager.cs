0d45fe0e84735d94fdcd4a403948e5b57b4bdc34
steamb23 <steamb23@gmail.com>
2015-10-25 00:52:24 +0900

    PhonemeArrayManager에서 필요없는 기능들은 모두 삭제 close #4

--- KoreanUtility/Hangul/PhonemeArrayManager.cs
+++ KoreanUtility/Hangul/PhonemeArrayManager.cs
@@ -11,7 +11,7 @@ namespace SteamB23.KoreanUtility.Hangul
     public static class PhonemeArrayManager
     {
         /// <summary>
-        /// Phoneme배열에서 source 문자들을 조합한 문자열을 가져옵니다.
+        /// Phoneme배열에서 source 문자들을 모아 문자열을 가져옵니다.
         /// </summary>
         /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
         /// <returns>source 문자들을 조합한 문자열입니다.</returns>
@@ -25,7 +25,7 @@ namespace SteamB23.KoreanUtility.Hangul
             return tempStringBuilder.ToString();
         }
         /// <summary>
-        /// Phoneme배열에서 초,중,종성 문자를 조합한 문자열을 가져옵니다.
+        /// Phoneme배열에서 초,중,종성 문자를 모아 문자열을 가져옵니다.
         /// </summary>
         /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
         /// <returns>initial, peak, final 문자들을 조합한 문자열입니다.</returns>
@@ -37,63 +37,6 @@ namespace SteamB23.KoreanUtility.Hangul
                 if (ftemp.initialConsonant != '\0' || ftemp.medialVowel != '\0')
                 {
                     tempStringBuilder.Append(ftemp.ToString());
-                }
-                else
-                    tempStringBuilder.Append(ftemp.source);
-            }
-            return tempStringBuilder.ToString();
-        }
-        /// <summary>
-        /// Phoneme배열에서 초성 문자를 조합한 문자열을 가져옵니다.
-        /// </summary>
-        /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
-        /// <returns>initial 문자들을 조합한 문자열입니다.</returns>
-        public static string GetInitialConsonantString(this Phoneme[] phonemes)
-        {
-            StringBuilder tempStringBuilder = new StringBuilder();
-            foreach (var ftemp in phonemes)
-            {
-                if (ftemp.initialConsonant == '\0')
-                {
-                    tempStringBuilder.Append(ftemp.initialConsonant);
-                }
-                else
-                    tempStringBuilder.Append(ftemp.source);
-            }
-            return tempStringBuilder.ToString();
-        }
-        /// <summary>
-        /// Phoneme배열에서 중성 문자를 조합한 문자열을 가져옵니다.
-        /// </summary>
-        /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
-        /// <returns>peak 문자들을 조합한 문자열입니다.</returns>
-        public static string GetMedialVowelString(this Phoneme[] phonemes)
-        {
-            StringBuilder tempStringBuilder = new StringBuilder();
-            foreach (var ftemp in phonemes)
-            {
-                if (ftemp.medialVowel == '\0')
-                {
-                    tempStringBuilder.Append(ftemp.medialVowel);
-                }
-                else
-                    tempStringBuilder.Append(ftemp.source);
-            }
-            return tempStringBuilder.ToString();
-        }
-        /// <summary>
-        /// Phoneme배열에서 종성 문자를 조합한 문자열을 가져옵니다.
-        /// </summary>
-        /// <param name="phonemes">대상 Phoneme 배열입니다.</param>
-        /// <returns>final 문자들을 조합한 문자열입니다.</returns>
-        public static string GetFinalConsonantString(this Phoneme[] phonemes)
-        {
-            StringBuilder tempStringBuilder = new StringBuilder();
-            foreach (var ftemp in phonemes)
-            {
-                if (ftemp.finalConsonant == '\0')
-                {
-                    tempStringBuilder.Append(ftemp.finalConsonant);
                 }
                 else
                     tempStringBuilder.Append(ftemp.source);
